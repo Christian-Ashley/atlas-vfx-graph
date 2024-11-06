e1f2f5f5bf69ff2cd51d665e37ccbbd0680d9a9c
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2024-11-06 14:44:07 -0600

    1

---
+++ beat-detection/Assets/scripts/audio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent (GetComponent<AudioSource>())]
+public class audio : MonoBehaviour
+{
+    AudioSource _audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audiosource = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource ();
+    }
+    void GetSpectrumAudioSource()
+    {
+        _audiosource.GetSpectrumData()
+    }
+}
